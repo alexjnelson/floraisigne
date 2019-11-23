1d27c667e7d8796c0f245cdecd1dd7df987d4551
alexjnelson <alexj.nelson9@gmail.com>
2019-11-23 13:40:29 -0500

    Added generateWord
    Theoretical full functionality of generateWord function - need to link to css in the master

--- public/functions/basicFunctions.js
+++ public/functions/basicFunctions.js
@@ -6,6 +6,8 @@ function generateWord(scrambled = false){
 	let word = randomWords[i];
 	let letters = [];
 
+	let letterListElement = document.getElementById(scrambled ? "skills-letter-list" : "letter-list");
+
 	for (ch of word){
 		letters.push({letter: ch, img: getSign(ch)});
 	}
@@ -18,15 +20,33 @@ function generateWord(scrambled = false){
 		imEl.id = letter + "im";
 		imEl.src=getSign(letter);
 
+		let letterWidth = letterListElement.style.width.replace("px","") / letters.length;
+		let letterHeight = 1.313 * letterWidth;
+
+		el.style.height = (letterHeight*0.1) + "px";
+		el.style.width = letterWidth + "px";
+
+		imEl.style.height = (letterHeight*0.9) + "px";
+		imEl.style.width = letterWidth + "px";
+
 		letterElements.push(el);
 		letterImgElements.push(imEl);
-
-
 	}
 
-	
+	if (scrambled) shuffle(letterImgElements);
+
+	for (let i = 0 ; i < letters.length ; i++){
+		letterListElement.appendChild(letterImgElements[i]);
+		letterListElement.appendChild(letterElements[i]);
+	}
 
 }
+
+function shuffle(arr){
+	// shuffle array
+	return arr;
+}
+
 
 function dragSign(ev){
 
