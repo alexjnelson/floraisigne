7f9f476bcda58dd296c1c0fb2fbc0e791ceb6b40
alexjnelson <alexj.nelson9@gmail.com>
2019-11-23 12:58:43 -0500

    Added functions
    Added functions including getting word list, generate the word, and WIP creating elements for letters. Also added words.json

--- public/functions/basicFunctions.js
+++ public/functions/basicFunctions.js
@@ -1,7 +1,34 @@
-const wordFile = require('./words.json');
+const randomWords = require('./words.json').words;
+var letterElements = [], letterImgElements = [];
 
 function generateWord(scrambled = false){
+	let i = Math.floor(Math.random()*randomWords.length);
+	let word = randomWords[i];
+	let letters = [];
+
+	for (ch of word){
+		letters.push({letter: ch, img: getSign(ch)});
+	}
+
+	for (letter of letters){
+		letterElements.push(document.createElement("p"));
+		letterImgElements.push(document.createElement("img"));
+	}
+
+	
 
 }
 
-console.log(wordFile);
+function dragSign(ev){
+
+}
+
+function dropSign(ev){
+
+}
+
+function getSign(ch){
+
+}
+
+generateWord();
