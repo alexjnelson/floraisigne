a0e2d0c97c64f6b72b54fe991e1d02e000e51c74
Luke-F-Harris <luke-f-harris@users.noreply.github.com>
2019-11-23 12:58:00 -0500

    changes

--- public/functions/speech.js
+++ public/functions/speech.js
@@ -5,6 +5,7 @@ function record() {
     recognition.onresult = function(event) {
         console.log(event); 
         document.getElementById('speechText').value = event.results[0][0].transcript.split()[0];
+        console.log(document.getElementById('speechText').value = event.results[0][0].transcript.split(" ")[0]);
     }
 
     recognition.start();
