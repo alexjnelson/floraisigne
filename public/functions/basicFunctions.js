af2c3a75efc6660899da095a6c0da0c98843d392
alexjnelson <alexj.nelson9@gmail.com>
2019-11-24 04:34:45 -0500

    yes

--- public/functions/basicFunctions.js
+++ public/functions/basicFunctions.js
@@ -4,10 +4,6 @@ const randomWords = ["ant", "fish","pie","bat","frog","seal","bear","goat","ship
 		"gold","sky","car","hat","star","cat","lake","stop","cow","leaf","sun","day","night",
 		"lion","dog","map","tree","duck","nest","truck"];
 var letterElements = [], letterImgElements = [], letterDivs = [];
-<<<<<<< Updated upstream
-
-function generateWord(word){
-=======
 
 var signDivs;
 
@@ -110,8 +106,8 @@ function search(word)
 
 
 function generateScrambledWord(word){
->>>>>>> Stashed changes
 	let oldWordImg = document.getElementById("wordImg");
+	signDivs = [];
 
 	if (oldWordImg !== null){
 		oldWordImg.remove();
@@ -120,7 +116,6 @@ function generateScrambledWord(word){
 	if (word === undefined) {
 		let i = Math.floor(Math.random()*randomWords.length);
 		word = randomWords[i];
-		console.log("Reached");
 	}
 
 	console.log(word);
@@ -146,10 +141,17 @@ function generateScrambledWord(word){
     	asl.removeChild(asl.firstChild);
   	}
 
+  	let signbank = document.getElementById("signbank");
+  	while(signbank.firstChild){
+  		signbank.removeChild(signbank.firstChild);
+  	}
+
   	let letterWidth=(90/word.length);
 	let padding = (95-word.length*letterWidth)/(word.length+1);
 	letterWidth+="%";
 	console.log(padding);
+
+	let handSigns = [];
 
 	for (let i = 0 ; i < word.length ; i++){
 		let letterDiv = document.createElement("div");
@@ -161,15 +163,31 @@ function generateScrambledWord(word){
 		letterDiv.style.marginTop="4%";
 
 		let signDiv = document.createElement("div");
-		signDiv.id = "sign" + i;
+		signDiv.id = "sign" + i + word.charAt(i);
 		signDiv.style.width="100%";
 		signDiv.style.height="80%";
 		signDiv.style.position="relative";
+		signDivs.push(signDiv);
+
+		signDiv.addEventListener("drop", dragdrop);
+		signDiv.addEventListener("dragover", dragover);
+
+		let bankDiv = document.createElement("div");
+		bankDiv.style.marginLeft = i === 0 ? (padding*2.5) +"%" : padding + "%";
+		bankDiv.style.width=letterWidth;
+		bankDiv.style.height="65%";
+		bankDiv.style.display="inline-flex";
+		bankDiv.style.marginTop="4%";
 
 		let signImg = document.createElement("img");
+		signImg.id = "image" + i + word.charAt(i);
 		signImg.src=getSign(word.charAt(i));
 		signImg.style.height="100%";
 		signImg.style.width="100%";
+
+		signImg.draggable="true";
+		signImg.addEventListener("dragstart", dragstart);
+		signImg.addEventListener("dragend", dragend);
 
 		let letter = document.createElement("div");
 		letter.innerHTML = word.charAt(i);
@@ -179,36 +197,74 @@ function generateScrambledWord(word){
 		letter.style.textAlign="center";
 		letter.style.fontSize="30px";
 
-
 		asl.appendChild(letterDiv);
 		letterDiv.appendChild(signDiv);
-		signDiv.appendChild(signImg);
+		signbank.appendChild(bankDiv);
+		handSigns.push(signImg);
 		letterDiv.appendChild(letter);
 	} 
 
+	shuffle(handSigns);
+
+	let nodes = signbank.childNodes;
+	for (let i = 0 ; i < nodes.length ; i++){
+		nodes[i].appendChild(handSigns[i]);
+	}
+
+	newP = document.createElement("p");
+	signbank.appendChild(newP);
+
 }
+
+
+function dragstart(){
+	console.log("start");
+	held = this;
+	console.log(held);
+}
+
+function dragdrop(){
+	console.log("dropped");
+
+	if (this.firstChild){
+		held.parentNode.appendChild(this.firstChild);
+		this.appendChild(held);
+	}
+	else{
+		this.appendChild(held);
+	}
+
+	let c = 0;
+	for (let signDiv of signDivs){
+		if (signDiv.id.charAt(5) !== signDiv.firstChild.id.charAt(6)){
+			c++;
+		}
+	}
+
+	if (c===0){
+		newP.innerHTML="Correct! Way to go!";
+		for (signDiv of signDivs){
+			signDiv.firstChild.draggable="false";
+		}
+	}
+	else {
+		newP.innerHTML= c + " signs are still incorrect!";
+	}
+
+}
+
+function dragend(){
+	console.log("end");
+
+	held = null;
+}
+
+function dragover(e){
+	e.preventDefault();
+}
+
+
 
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5);
-}
-
-
-function dragSign(ev){
-
-}
-
-function dropSign(ev){
-
-}
-
-function search(word)
-{	
-	var key = "AIzaSyDJfvKrb8ui7Bu5KhpMDyTfalLv6POs614"
-	var id = "005443348412993502233:cklwqdwacj7"
-	theUrl = `https://www.googleapis.com/customsearch/v1?key=${key}&cx=${id}&q=${word}&searchType=image&fileType=jpg&imgSize=small&alt=json`
-
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
 }
