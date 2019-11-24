db8336bde2b436d50fd30e1e9918426fe3688db1
alexjnelson <alexj.nelson9@gmail.com>
2019-11-24 04:26:37 -0500

    yes

--- public/functions/basicFunctions.js
+++ public/functions/basicFunctions.js
@@ -8,7 +8,18 @@ var letterElements = [], letterImgElements = [];
 function generateWord(scrambled = false, word=""){
 	let i = Math.floor(Math.random()*randomWords.length);
 
+<<<<<<< Updated upstream
+=======
+var signDivs;
+
+var newP;
+
+var held;
+
+function generateWord(word){
+>>>>>>> Stashed changes
 	let oldWordImg = document.getElementById("wordImg");
+	signDivs = [];
 
 	if (oldWordImg !== null){
 		oldWordImg.remove();
@@ -82,18 +93,6 @@ function generateWord(scrambled = false, word=""){
 
 }
 
-function shuffle(array) {
-  array.sort(() => Math.random() - 0.5);
-}
-
-
-function dragSign(ev){
-
-}
-
-function dropSign(ev){
-
-}
 
 function search(word)
 {	
@@ -105,4 +104,171 @@ function search(word)
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
     xmlHttp.send( null );
     return xmlHttp.responseText;
+}
+
+
+
+
+function generateScrambledWord(word){
+	let oldWordImg = document.getElementById("wordImg");
+	signDivs = [];
+
+	if (oldWordImg !== null){
+		oldWordImg.remove();
+	}
+
+	if (word === undefined) {
+		let i = Math.floor(Math.random()*randomWords.length);
+		word = randomWords[i];
+	}
+
+	console.log(word);
+
+	let wordImg = document.createElement("img");
+	let wordImgDiv = document.getElementById("image");
+	//let imgURL = JSON.parse(search(word)).items[0].link;
+	imgURL = "https://img.cinemablend.com/filter:scale/quill/e/5/c/2/4/9/e5c2494b7fec13181defbc8afe66a7a3bee5bae0.jpg?fw=1200";
+
+	console.log(imgURL);
+	wordImg.src=imgURL;
+
+	wordImg.style.height = "100%";
+	wordImg.style.width= "100%";
+	wordImg.style.borderRadius="20px";
+	wordImg.id = "wordImg";
+
+	wordImgDiv.appendChild(wordImg);	
+
+
+	let asl = document.getElementById("ASL");
+	while (asl.firstChild) {
+    	asl.removeChild(asl.firstChild);
+  	}
+
+  	let signbank = document.getElementById("signbank");
+  	while(signbank.firstChild){
+  		signbank.removeChild(signbank.firstChild);
+  	}
+
+  	let letterWidth=(90/word.length);
+	let padding = (95-word.length*letterWidth)/(word.length+1);
+	letterWidth+="%";
+	console.log(padding);
+
+	let handSigns = [];
+
+	for (let i = 0 ; i < word.length ; i++){
+		let letterDiv = document.createElement("div");
+		letterDiv.id = "letter" + i;
+		letterDiv.style.marginLeft = i === 0 ? (padding*2.5) +"%" : padding + "%";
+		letterDiv.style.width=letterWidth;
+		letterDiv.style.height="65%";
+		letterDiv.style.display="inline-flex";
+		letterDiv.style.marginTop="4%";
+
+		let signDiv = document.createElement("div");
+		signDiv.id = "sign" + i + word.charAt(i);
+		signDiv.style.width="100%";
+		signDiv.style.height="80%";
+		signDiv.style.position="relative";
+		signDivs.push(signDiv);
+
+		signDiv.addEventListener("drop", dragdrop);
+		signDiv.addEventListener("dragover", dragover);
+
+		let bankDiv = document.createElement("div");
+		bankDiv.style.marginLeft = i === 0 ? (padding*2.5) +"%" : padding + "%";
+		bankDiv.style.width=letterWidth;
+		bankDiv.style.height="65%";
+		bankDiv.style.display="inline-flex";
+		bankDiv.style.marginTop="4%";
+
+		let signImg = document.createElement("img");
+		signImg.id = "image" + i + word.charAt(i);
+		signImg.src=getSign(word.charAt(i));
+		signImg.style.height="100%";
+		signImg.style.width="100%";
+
+		signImg.draggable="true";
+		signImg.addEventListener("dragstart", dragstart);
+		signImg.addEventListener("dragend", dragend);
+
+		let letter = document.createElement("div");
+		letter.innerHTML = word.charAt(i);
+		letter.style.position="absolute";
+		letter.style.width=letterWidth;
+		letter.style.top="67%";
+		letter.style.textAlign="center";
+		letter.style.fontSize="30px";
+
+		asl.appendChild(letterDiv);
+		letterDiv.appendChild(signDiv);
+		signbank.appendChild(bankDiv);
+		handSigns.push(signImg);
+		letterDiv.appendChild(letter);
+	} 
+
+	shuffle(handSigns);
+
+	let nodes = signbank.childNodes;
+	for (let i = 0 ; i < nodes.length ; i++){
+		nodes[i].appendChild(handSigns[i]);
+	}
+
+	newP = document.createElement("p");
+	signbank.appendChild(newP);
+
+}
+
+
+function dragstart(){
+	console.log("start");
+	held = this;
+	console.log(held);
+}
+
+function dragdrop(){
+	console.log("dropped");
+
+	if (this.firstChild){
+		held.parentNode.appendChild(this.firstChild);
+		this.appendChild(held);
+	}
+	else{
+		this.appendChild(held);
+	}
+
+	let c = 0;
+	for (let signDiv of signDivs){
+		if (signDiv.id.charAt(5) !== signDiv.firstChild.id.charAt(6)){
+			c++;
+		}
+	}
+
+	if (c===0){
+		newP.innerHTML="Correct! Way to go!";
+		for (signDiv of signDivs){
+			signDiv.firstChild.draggable="false";
+		}
+	}
+	else {
+		newP.innerHTML= c + " signs are still incorrect!";
+	}
+
+}
+
+function dragend(){
+	console.log("end");
+
+	held = null;
+}
+
+function dragover(e){
+	e.preventDefault();
+}
+
+
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
 }
